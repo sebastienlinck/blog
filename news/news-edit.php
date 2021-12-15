c1010fe48671b55a757b1ce1919e9747018c65d4
Sébastien Linck <linck.sebastien@gmail.Com>
2021-12-15 10:47:57 +0100

    indentation

--- news/news-edit.php
+++ news/news-edit.php
@@ -6,58 +6,57 @@
 	}
 ?>
 <!doctype html>
-<html lang="fr">
+<html lang = "fr">
 	<head>
-		<meta charset="utf-8">
+		<meta charset = "utf-8">
 		<title>Modification d'une actualité</title>
-		<link rel="stylesheet" href="../css/style.css">
-		
+		<link rel = "stylesheet" href = "../css/style.css">
 	</head>
 	<body>
-		<div id="page">
+		<div id = "page">
 			<h1>Modification d'une actualité</h1>
 			<?php
 				include('../config/bdd.php');				
 				include('../config/tools.php');
-				$lien=mysqli_connect(SERVEUR,LOGIN,MDP,BASE);
-				$num=nettoyage($lien,$_REQUEST['num']);
+				$lien = mysqli_connect(SERVEUR, LOGIN, MDP, BASE);
+				$num = nettoyage($lien, $_REQUEST['num']);
 				if(isset($_REQUEST['modifier']))
 				{
-					$title=nettoyage($lien,$_REQUEST['title']);
-					$content=nettoyage($lien,$_REQUEST['content']);
-					if($_FILES['image']['name']=="")
+					$title = nettoyage($lien, $_REQUEST['title']);
+					$content = nettoyage($lien, $_REQUEST['content']);
+					if($_FILES['image']['name'] = = "")
 					{
-						$req="UPDATE news SET title='$title', content='$content' WHERE idn=$num";
+						$req = "UPDATE news SET title = '$title', content = '$content' WHERE idn = $num";
 					}
 					else
 					{
-						$extensionsvalides=array('gif','jpg','png','jpeg','svg');
-						$extension=strtolower(substr(strrchr($_FILES['image']['name'],"."),1));
+						$extensionsvalides = array('gif', 'jpg', 'png', 'jpeg', 'svg');
+						$extension = strtolower(substr(strrchr($_FILES['image']['name'], "."), 1));
 						if(in_array($extension, $extensionsvalides))
 						{
-							$destination="../images/".uniqid().".$extension";
-							$envoi=move_uploaded_file($_FILES['image']['tmp_name'],$destination);
+							$destination = "../images/".uniqid().".$extension";
+							$envoi = move_uploaded_file($_FILES['image']['tmp_name'], $destination);
 							if(!$envoi)
 							{
 								echo "Erreur de transfert<br>";
-								$destination="";
+								$destination = "";
 							}
 						}
 						else
 						{
 							echo "Pas d'image ou image invalide<br>";
-							$destination="";
+							$destination = "";
 						}
-						if($destination=="")
+						if($destination = = "")
 						{
-							$req="UPDATE news SET title='$title', content='$content' WHERE idn=$num";
+							$req = "UPDATE news SET title = '$title', content = '$content' WHERE idn = $num";
 						}
 						else
 						{
-							$req="UPDATE news SET title='$title', content='$content', image='$destination' WHERE idn=$num";
+							$req = "UPDATE news SET title = '$title', content = '$content', image = '$destination' WHERE idn = $num";
 						}
 					}
-					$res=mysqli_query($lien,$req);
+					$res = mysqli_query($lien, $req);
 					if(!$res)
 					{
 						echo "Erreur SQL: $req<br>".mysqli_error($lien);
@@ -66,42 +65,42 @@
 					else
 					{
 						echo "Actualité modifiée<br>";
-						if(($_REQUEST['ancienneimage']!="")and($_FILES['image']['name']!=""))
+						if(($_REQUEST['ancienneimage']! = "")and($_FILES['image']['name']! = ""))
 						{
 							unlink($_REQUEST['ancienneimage']);
 						}
 					}
 				}
-				$req="SELECT * FROM news WHERE idn=$num";
-				$res=mysqli_query($lien,$req);
+				$req = "SELECT * FROM news WHERE idn = $num";
+				$res = mysqli_query($lien, $req);
 				if(!$res)
 				{
 					echo "Erreur SQL: $req<br>".mysqli_error($lien);
 				}
 				else
 				{
-					$infos=mysqli_fetch_array($res);
-					if(($infos['author']!=$_SESSION['idu'])and($_SESSION['admin']==0))
+					$infos = mysqli_fetch_array($res);
+					if(($infos['author']! = $_SESSION['idu'])and($_SESSION['admin'] = = 0))
 					{
 						mysqli_close($lien);
 						header("Location: ../index.php");
 						exit;
 					}
 				?>
-				<form method="post" enctype="multipart/form-data">
-					<label>Titre : </label><input type="text" name="title" value="<?php echo $infos['title'];?>"><br>
-					<label>Contenu : </label><textarea name="content"><?php echo $infos['content'];?></textarea><br>
-					<label>Ancienne image : </label><img src="<?php echo $infos['image'];?>"><br>
-					<label>Nouvelle image : </label><input type="file" name="image"><br>
-					<input type="hidden" name="num" value="<?php echo $num;?>">
-					<input type="hidden" name="ancienneimage" value="<?php echo $infos['image'];?>">
-					<input type="submit" name="modifier" value="Modifier l'actualité"><br>
+				<form method = "post" enctype = "multipart/form-data">
+					<label>Titre : </label><input type = "text" name = "title" value = "<?php echo $infos['title'];?>"><br>
+					<label>Contenu : </label><textarea name = "content"><?php echo $infos['content'];?></textarea><br>
+					<label>Ancienne image : </label><img src = "<?php echo $infos['image'];?>"><br>
+					<label>Nouvelle image : </label><input type = "file" name = "image"><br>
+					<input type = "hidden" name = "num" value = "<?php echo $num;?>">
+					<input type = "hidden" name = "ancienneimage" value = "<?php echo $infos['image'];?>">
+					<input type = "submit" name = "modifier" value = "Modifier l'actualité"><br>
 				</form>
 				<?php
 				}
 				mysqli_close($lien);
 			?>
-			<a href="../">Accueil</a>
+			<a href = "../">Accueil</a>
 		</div>
 	</body>
 </html>	
--- users/registration.php
+++ users/registration.php
@@ -1,66 +1,65 @@
 <!doctype html>
-<html lang="fr">
+<html lang = "fr">
 	<head>
-		<meta charset="utf-8">
+		<meta charset = "utf-8">
 		<title>Inscription</title>
-		<link rel="stylesheet" href="../css/style.css">
-		<script src="../js/scripts.js"></script>
+		<link rel = "stylesheet" href = "../css/style.css">
+		<script src = "../js/scripts.js"></script>
 	</head>
 	<body>
-		<div id="page">
+		<div id = "page">
 			<h1>Inscription</h1>
-			<form method="post">
-				<label>Nom : </label><input type="text" name="lastname" required><br>
-				<label>Prénom : </label><input type="text" name="firstname" required><br>
-				<label>Courriel : </label><input type="email" name="email1" id="email1" required onkeyup="verif('email1','email2')"><br>
-				<label>Confirmation courriel : </label><input type="email" name="email2" id="email2" required onkeyup="verif('email1','email2')"><br>
-				<label>Mot de passe : </label><input type="password" name="pwd1" id="pwd1" required onkeyup="verif('pwd1','pwd2')"><br>
-				<label>Confirmation mot de passe : </label><input type="password" name="pwd2" id="pwd2" required onkeyup="verif('pwd1','pwd2')"><br>
-				<input type="submit" name="inscription" value="Inscription"><br>
+			<form method = "post">
+				<label>Nom : </label><input type = "text" name = "lastname" required><br>
+				<label>Prénom : </label><input type = "text" name = "firstname" required><br>
+				<label>Courriel : </label><input type = "email" name = "email1" id = "email1" required onkeyup = "verif('email1', 'email2')"><br>
+				<label>Confirmation courriel : </label><input type = "email" name = "email2" id = "email2" required onkeyup = "verif('email1', 'email2')"><br>
+				<label>Mot de passe : </label><input type = "password" name = "pwd1" id = "pwd1" required onkeyup = "verif('pwd1', 'pwd2')"><br>
+				<label>Confirmation mot de passe : </label><input type = "password" name = "pwd2" id = "pwd2" required onkeyup = "verif('pwd1', 'pwd2')"><br>
+				<input type = "submit" name = "inscription" value = "Inscription"><br>
 			</form>
 			<?php
 				if(isset($_REQUEST['inscription']))
 				{
 					include('../config/bdd.php');
 					include('../config/tools.php');
-					$lien=mysqli_connect(SERVEUR,LOGIN,MDP,BASE);
-					$lastname=nettoyage($lien,$_REQUEST['lastname']);
-					$firstname=nettoyage($lien,$_REQUEST['firstname']);
-					$email1=nettoyage($lien,$_REQUEST['email1']);
-					$email2=nettoyage($lien,$_REQUEST['email2']);
-					$pwd1=$_REQUEST['pwd1'];
-					$pwd2=$_REQUEST['pwd2'];
-					
-					if (($pwd1==$pwd2) and ($email1==$email2))
+					$lien = mysqli_connect(SERVEUR, LOGIN, MDP, BASE);
+					$lastname = nettoyage($lien, $_REQUEST['lastname']);
+					$firstname = nettoyage($lien, $_REQUEST['firstname']);
+					$email1 = nettoyage($lien, $_REQUEST['email1']);
+					$email2 = nettoyage($lien, $_REQUEST['email2']);
+					$pwd1 = $_REQUEST['pwd1'];
+					$pwd2 = $_REQUEST['pwd2'];
+					if (($pwd1 = = $pwd2) and ($email1 = = $email2))
 					{
-						$req="SELECT * FROM users";
-						$res=mysqli_query($lien,$req);
+						$req = "SELECT * FROM users";
+						$res = mysqli_query($lien, $req);
 						if(!$res)
 						{
 							echo "Erreur SQL: $req<br>".mysqli_error($lien);
 						}
 						else
 						{
-							$first_user=0;
-							if (mysqli_num_rows($res)==0)
+							$first_user = 0;
+							if (mysqli_num_rows($res) = = 0)
 							{
-								$first_user=1;
+								$first_user = 1;
 							}
 						}
-						$req="SELECT * FROM users WHERE email='$email1'";
-						$res=mysqli_query($lien,$req);
+						$req = "SELECT * FROM users WHERE email = '$email1'";
+						$res = mysqli_query($lien, $req);
 						if(!$res)
 						{
 							echo "Erreur SQL: $req<br>".mysqli_error($lien);
 						}
 						else
 						{
-							$existe=mysqli_num_rows($res);
-							if (($existe==0) and ($pwd1==$pwd2))
+							$existe = mysqli_num_rows($res);
+							if (($existe = = 0) and ($pwd1 = = $pwd2))
 							{
-								$pwd=password_hash($pwd1, PASSWORD_DEFAULT);
-								$req="INSERT INTO users VALUES(NULL,'$firstname','$lastname','$email1','$pwd',$first_user,$first_user)";
-								$res=mysqli_query($lien,$req);
+								$pwd = password_hash($pwd1, PASSWORD_DEFAULT);
+								$req = "INSERT INTO users VALUES(NULL, '$firstname', '$lastname', '$email1', '$pwd', $first_user, $first_user)";
+								$res = mysqli_query($lien, $req);
 								if(!$res)
 								{
 									echo "Erreur SQL: $req<br>".mysqli_error($lien);
@@ -69,21 +68,21 @@
 								{
 									echo "Inscription réussie<br>";
 								}
-								}
-								else 
-								{
-									echo "Adresse email déjà utilisée<br>";
-								}
-							}	
-						}
-						else
-						{
-							echo "Les mots de passe ou les courriels sont différents<br>";
-						}
-						mysqli_close($lien);
+							}
+							else 
+							{
+								echo "Adresse email déjà utilisée<br>";
+							}
+						}	
 					}
-				?>
-				<a href="../">Accueil</a>
-			</div>
-		</body>
-	</html>	
+					else
+					{
+						echo "Les mots de passe ou les courriels sont différents<br>";
+					}
+					mysqli_close($lien);
+				}
+			?>
+			<a href = "../">Accueil</a>
+		</div>
+	</body>
+</html>	
